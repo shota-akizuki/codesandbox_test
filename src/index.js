e0d90aed2812ac0f985d0c5e2760eeb1f76282c2
Shota <64979430+shota-akizuki@users.noreply.github.com>
2021-03-06 20:16:46 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -53,15 +53,46 @@
 
 //テンプレート文字列
 
-const name = "じゃけぇ";
-const age = 28;
+// const name = "じゃけぇ";
+// const age = 28;
 
-//「私の名前はじゃけぇです。年齢は２８歳です。」
-//従来の方法
-const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// //「私の名前はじゃけぇです。年齢は２８歳です。」
+// //従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
 
-console.log(message1);
-//テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message1);
+// //テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
 
-console.log(message2);
+// console.log(message2);
+/**
+ * アロー関数
+ */
+//従来の関数
+// function func1(str) {
+//   return str;
+// }
+
+const func1 = function (str) {
+  return str;
+};
+
+console.log(func1("func１です"));
+
+//アロー関数
+const func2 = (str) => {
+  return str;
+};
+//以下等価
+// const func2 = (str) => str;
+
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+
+//以下等価
+// const func3 = (num1, num2) => num1 + num2;
+
+console.log(func3(10, 23));
